abf0b0f09f1399186214ec2b1d5229acb701fb3e
annashamray <anna.shamray@maykinmedia.nl>
2020-09-29 18:47:02 +0200

    :bug: add SITE_ID to config

--- src/objects/conf/base.py
+++ src/objects/conf/base.py
@@ -367,3 +367,5 @@ ELASTIC_APM = {
     "SECRET_TOKEN": os.getenv("ELASTIC_APM_SECRET_TOKEN", "default"),
     "SERVER_URL": os.getenv("ELASTIC_APM_SERVER_URL", "http://example.com"),
 }
+
+SITE_ID = os.getenv("SITE_ID", 1)
