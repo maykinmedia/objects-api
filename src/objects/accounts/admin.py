6879e153a0642a7725f1575379214381fb188c8b
annashamray <anna.shamray@maykinmedia.nl>
2020-10-12 17:36:22 +0200

    :sparkles: Ref. #78 -- create ObjectPermission model to store permissions based on object types

--- src/objects/accounts/admin.py
+++ src/objects/accounts/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import ugettext_lazy as _
 
 from hijack_admin.admin import HijackUserAdminMixin
 
-from .models import User
+from .models import ObjectPermission, User
 
 
 @admin.register(User)
 class _UserAdmin(UserAdmin, HijackUserAdminMixin):
     list_display = UserAdmin.list_display + ("hijack_field",)
+    fieldsets = UserAdmin.fieldsets + (
+        (_("Object permissions"), {"fields": ("object_permissions",)}),
+    )
+    raw_id_fields = ("object_permissions",)
+
+
+@admin.register(ObjectPermission)
+class ObjectPermissionAdmin(admin.ModelAdmin):
+    list_display = ("object_type", "mode")
