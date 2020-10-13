978f9055c51b3f15723657f62288566a41a4c0bb
annashamray <anna.shamray@maykinmedia.nl>
2020-10-13 17:38:51 +0200

    :white_check_mark: test object type permissions

--- src/objects/tests/test_geo_search.py
+++ src/objects/tests/test_geo_search.py
@@ -4,24 +4,44 @@ from django.urls import reverse, reverse_lazy
 from rest_framework import status
 from rest_framework.test import APITestCase
 
+from objects.accounts.constants import PermissionModes
+from objects.accounts.tests.factories import ObjectPermissionFactory
 from objects.core.tests.factores import ObjectRecordFactory
 from objects.utils.test import TokenAuthMixin
 
 from .constants import GEO_WRITE_KWARGS, POLYGON_AMSTERDAM_CENTRUM
 
 OBJECT_TYPE = "https://example.com/objecttypes/v1/types/a6c109"
+OTHER_OBJECT_TYPE = "https://example.com/objecttypes/v1/types/qwe109"
 
 
 class GeoSearchTests(TokenAuthMixin, APITestCase):
     url = reverse_lazy("object-search")
 
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+
+        ObjectPermissionFactory(
+            object_type=OBJECT_TYPE, mode=PermissionModes.read_only, users=[cls.user]
+        )
+        ObjectPermissionFactory(
+            object_type=OTHER_OBJECT_TYPE,
+            mode=PermissionModes.read_only,
+            users=[cls.user],
+        )
+
     def test_filter_within(self):
         # in district
-        record = ObjectRecordFactory.create(geometry=Point(4.905289, 52.369918))
+        record = ObjectRecordFactory.create(
+            object__object_type=OBJECT_TYPE, geometry=Point(4.905289, 52.369918)
+        )
         # outside of district
-        ObjectRecordFactory.create(geometry=Point(4.905650, 52.357621))
+        ObjectRecordFactory.create(
+            object__object_type=OBJECT_TYPE, geometry=Point(4.905650, 52.357621)
+        )
         # no geo set
-        ObjectRecordFactory.create()
+        ObjectRecordFactory.create(object__object_type=OBJECT_TYPE)
 
         response = self.client.post(
             self.url,
@@ -50,7 +70,9 @@ class GeoSearchTests(TokenAuthMixin, APITestCase):
         record = ObjectRecordFactory.create(
             geometry=Point(4.905289, 52.369918), object__object_type=OBJECT_TYPE
         )
-        ObjectRecordFactory.create(geometry=Point(4.905289, 52.369918))
+        ObjectRecordFactory.create(
+            geometry=Point(4.905289, 52.369918), object__object_type=OTHER_OBJECT_TYPE
+        )
 
         response = self.client.post(
             self.url,
