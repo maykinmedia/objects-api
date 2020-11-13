99149575d9ad73c2dbcbf123f038a61c8f948259
Anna Shamray <anna.shamray@maykinmedia.nl>
2020-11-13 16:23:49 +0100

    :construction_worker: Ref. #91 -- add github action to check quick start

---
+++ bin/quick_start.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -ex
+
+wget https://raw.githubusercontent.com/maykinmedia/objects-api/master/docker-compose-quickstart.yml -O docker-compose-qs.yml
+docker-compose -f docker-compose-qs.yml up -d
+docker-compose -f docker-compose-qs.yml exec web src/manage.py loaddata demodata
