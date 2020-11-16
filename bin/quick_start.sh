8d0ca47e44d10fa0f73c74b691e8db1fcc5f6bac
Anna Shamray <anna.shamray@maykinmedia.nl>
2020-11-16 16:40:47 +0100

    :green_heart: wait until database is ready in quick_start.sh

--- bin/quick_start.sh
+++ bin/quick_start.sh
@@ -4,4 +4,7 @@ set -ex
 
 wget https://raw.githubusercontent.com/maykinmedia/objects-api/master/docker-compose-quickstart.yml -O docker-compose-qs.yml
 docker-compose -f docker-compose-qs.yml up -d
+
+sleep 10
+
 docker-compose exec -T web src/manage.py loaddata demodata
