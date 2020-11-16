a786b3352a803d554abfd2818088207d89d72c18
Anna Shamray <anna.shamray@maykinmedia.nl>
2020-11-16 16:07:58 +0100

    :green_heart: fix tty in github aciton

--- bin/quick_start.sh
+++ bin/quick_start.sh
@@ -4,4 +4,4 @@ set -ex
 
 wget https://raw.githubusercontent.com/maykinmedia/objects-api/master/docker-compose-quickstart.yml -O docker-compose-qs.yml
 docker-compose -f docker-compose-qs.yml up -d
-docker-compose -f docker-compose-qs.yml exec web src/manage.py loaddata demodata
+docker-compose exec -T web src/manage.py loaddata demodata
