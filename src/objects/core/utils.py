9148aa2e428c40d952011f0d44a7a7a012c491d1
Anna Shamray <anna.shamray@maykinmedia.nl>
2020-12-08 19:45:08 +0100

    :sparkles: use zgw_consumers.Client to request object type

--- src/objects/core/utils.py
+++ src/objects/core/utils.py
@@ -1,9 +1,7 @@
-import json
-
 from django.core.exceptions import ValidationError
 
 import jsonschema
-import requests
+from zds_client.client import ClientError
 from zgw_consumers.models import Service
 
 
@@ -11,26 +9,16 @@ def check_objecttype(object_type, version, data):
     if not data:
         return
 
+    client = Service.get_client(object_type)
     objecttype_version_url = f"{object_type}/versions/{version}"
 
-    auth = Service.get_auth_header(object_type)
-    response = requests.get(objecttype_version_url, headers=auth)
-
-    if response.status_code == 404:
-        msg = f"{object_type} or version {version} doesn't exist."
-        raise ValidationError(msg)
-
     try:
-        response.raise_for_status()
-    except requests.exceptions.RequestException as exc:
+        response = client.retrieve("objectversion", url=objecttype_version_url)
+    except ClientError as exc:
         raise ValidationError(exc.args[0]) from exc
 
     try:
-        json_data = response.json()
-        schema = json_data["jsonSchema"]
-    except json.decoder.JSONDecodeError:
-        msg = f"{objecttype_version_url} returned invalid JSON."
-        raise ValidationError(msg)
+        schema = response["jsonSchema"]
     except KeyError:
         msg = f"{objecttype_version_url} does not appear to be a valid objecttype."
         raise ValidationError(msg)
