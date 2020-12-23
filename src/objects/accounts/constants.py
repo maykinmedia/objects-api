179b74d4be11cb90d38dc57d949b299bfe1fa817
Anna Shamray <anna.shamray@maykinmedia.nl>
2020-12-23 13:01:28 +0100

    :fire: remove ObjectPermision model and old permissions

--- src/objects/accounts/constants.py
+++
@@ -1,8 +0,0 @@
-from django.utils.translation import ugettext_lazy as _
-
-from djchoices import ChoiceItem, DjangoChoices
-
-
-class PermissionModes(DjangoChoices):
-    read_only = ChoiceItem("read_only", _("Read-only"))
-    read_and_write = ChoiceItem("read_and_write", _("Read and write"))
--- src/objects/accounts/tests/factories.py
+++ src/objects/accounts/tests/factories.py
@@ -1,7 +1,4 @@
-import factory
 import factory.fuzzy
-
-from ..constants import PermissionModes
 
 
 class UserFactory(factory.django.DjangoModelFactory):
@@ -9,21 +6,3 @@ class UserFactory(factory.django.DjangoModelFactory):
 
     class Meta:
         model = "accounts.User"
-
-
-class ObjectPermissionFactory(factory.django.DjangoModelFactory):
-    object_type = factory.Faker("url")
-    mode = factory.fuzzy.FuzzyChoice(choices=PermissionModes.values)
-
-    class Meta:
-        model = "accounts.ObjectPermission"
-
-    @factory.post_generation
-    def users(self, create, extracted, **kwargs):
-        # optional M2M, do nothing when no arguments are passed
-        if not create:
-            return
-
-        if extracted:
-            for user in extracted:
-                self.users.add(user)
