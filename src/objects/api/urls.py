aae751e268ab0de5cb0fedc60805014696727f88
Anna Shamray <anna.shamray@maykinmedia.nl>
2021-06-17 10:51:11 +0200

    :bug: fix urls on the landing page

--- src/objects/api/urls.py
+++ src/objects/api/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import include
 from django.urls import path
 
 urlpatterns = [
-    path("v1/", include("objects.api.v1.urls", namespace="v1")),
-    path("v2/", include("objects.api.v2.urls", namespace="v2")),
+    path("v1", include("objects.api.v1.urls", namespace="v1")),
+    path("v2", include("objects.api.v2.urls", namespace="v2")),
 ]
--- src/objects/api/v1/urls.py
+++ src/objects/api/v1/urls.py
@@ -17,18 +17,25 @@ router.register(r"objects", ObjectViewSet)
 app_name = "v1"
 
 urlpatterns = [
-    # schema
-    path(
-        "schema/openapi.yaml",
-        SpectacularYAMLAPIView.as_view(),
-        name="schema",
-    ),
-    path(
-        "schema/",
-        SpectacularRedocView.as_view(url_name="schema"),
-        name="schema-redoc",
-    ),
     path("", SpectacularJSONAPIView.as_view(), name="schema-json"),
-    # actual endpoints
-    path("", include(router.urls)),
+    path(
+        "/",
+        include(
+            [
+                # schema
+                path(
+                    "schema/openapi.yaml",
+                    SpectacularYAMLAPIView.as_view(),
+                    name="schema",
+                ),
+                path(
+                    "schema/",
+                    SpectacularRedocView.as_view(url_name="schema"),
+                    name="schema-redoc",
+                ),
+                # actual endpoints
+                path("", include(router.urls)),
+            ]
+        ),
+    ),
 ]
