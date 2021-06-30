ffc57c1a00c4d9cff7d7a584df95e6b2f416a4d2
Anna Shamray <anna.shamray@maykinmedia.nl>
2021-06-30 15:41:45 +0200

    :sparkles: add field based authorization to serializer

--- src/objects/utils/serializers.py
+++ src/objects/utils/serializers.py
@@ -1,7 +1,13 @@
-from django.utils.translation import ugettext_lazy as _
+import logging
+
+from django.db import models
 
 from glom import GlomError, glom
 from rest_framework import serializers
+
+from objects.token.constants import PermissionModes
+
+logger = logging.getLogger(__name__)
 
 
 def build_spec(fields) -> dict:
@@ -23,32 +29,67 @@ def build_spec_field(spec, name, value):
 class DynamicFieldsMixin:
     """
     this mixin allows selecting fields for serializer in the query param
-    It also supports nested fields which are serializers themselves.
+    It also supports nested fields.
     """
 
     def to_representation(self, instance):
         data = super().to_representation(instance)
 
+        allowed_fields = self.get_allowed_fields()
         query_fields = self.get_query_fields()
 
-        if not query_fields:
+        if not query_fields and allowed_fields == ["*"]:
             return data
 
-        spec = build_spec(query_fields)
-        try:
-            return glom(data, spec)
-        except GlomError as exc:
-            raise serializers.ValidationError(
-                f"'fields' query parameter has invalid values: {exc.args[0]}"
-            )
+        if allowed_fields == ["*"]:
+            allowed_data = data
+        else:
+            spec_allowed = build_spec(allowed_fields)
+            try:
+                allowed_data = glom(data, spec_allowed)
+            except GlomError as exc:
+                raise serializers.ValidationError(
+                    f"Fields in the configured authorization are absent in the data: {exc.args[0]}"
+                )
 
-    def get_query_fields(self) -> set:
+        if not query_fields:
+            result_data = allowed_data
+        else:
+            spec_query = build_spec(query_fields)
+            try:
+                result_data = glom(allowed_data, spec_query)
+            except GlomError as exc:
+                raise serializers.ValidationError(
+                    f"'fields' query parameter has invalid values: {exc.args[0]}"
+                )
+
+        return result_data
+
+    def get_query_fields(self) -> list:
         request = self.context.get("request")
         if not request or request.method != "GET":
-            return set()
+            return []
 
         fields = request.query_params.get("fields")
         if not fields:
-            return set()
+            return []
 
-        return set(field.strip() for field in fields.split(","))
+        return list(set(field.strip() for field in fields.split(",")))
+
+    def get_allowed_fields(self) -> list:
+        request = self.context.get("request")
+        object = self.instance
+
+        # if not instance -> create or update -> all fields are allowed
+        if not object or not request:
+            return ["*"]
+
+        # skip for list serializers, since it's check on its child level
+        if isinstance(object, models.QuerySet) or isinstance(object, list):
+            return ["*"]
+
+        permission = request.auth.get_permission_for_object_type(object.object_type)
+        if permission.mode == PermissionModes.read_only and permission.use_fields:
+            return permission.fields
+
+        return ["*"]
