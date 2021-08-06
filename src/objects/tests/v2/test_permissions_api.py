f677aeae71ade8ed411b01d8cee92d291c21555e
Anna Shamray <anna.shamray@maykinmedia.nl>
2021-08-06 15:58:38 +0200

    :white_check_mark: Ref. #81 -- test permissions endpoint

---
+++ src/objects/tests/v2/test_permissions_api.py
@@ -0,0 +1,92 @@
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from objects.token.constants import PermissionModes
+from objects.token.tests.factories import PermissionFactory
+from objects.utils.test import TokenAuthMixin
+
+from .utils import reverse
+
+OBJECT_TYPES_API = "https://example.com/objecttypes/v1/"
+
+
+class ObjectApiTests(TokenAuthMixin, APITestCase):
+    maxDiff = None
+
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+
+    def test_list_permissions(self):
+        permission1 = PermissionFactory.create(
+            token_auth=self.token_auth,
+            mode=PermissionModes.read_and_write,
+        )
+        permission2 = PermissionFactory.create(
+            token_auth=self.token_auth,
+            mode=PermissionModes.read_only,
+            use_fields=True,
+            fields={"1": ["url", "uuid"], "2": ["url", "record"]},
+        )
+
+        url = reverse("permission-list")
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        data = response.json()
+
+        self.assertEqual(
+            data,
+            {
+                "count": 2,
+                "next": None,
+                "previous": None,
+                "results": [
+                    {
+                        "type": permission1.object_type.url,
+                        "mode": PermissionModes.read_and_write,
+                        "use_fields": False,
+                        "fields": {},
+                    },
+                    {
+                        "type": permission2.object_type.url,
+                        "mode": "read_only",
+                        "use_fields": True,
+                        "fields": {"1": ["url", "uuid"], "2": ["url", "record"]},
+                    },
+                ],
+            },
+        )
+
+    def test_list_permissions_for_only_user(self):
+        permission1 = PermissionFactory.create(
+            token_auth=self.token_auth,
+            mode=PermissionModes.read_and_write,
+        )
+        # permission for other token
+        PermissionFactory.create(
+            mode=PermissionModes.read_and_write,
+        )
+
+        url = reverse("permission-list")
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        data = response.json()["results"]
+
+        self.assertEqual(len(data), 1)
+        self.assertEqual(
+            data,
+            [
+                {
+                    "type": permission1.object_type.url,
+                    "mode": PermissionModes.read_and_write,
+                    "use_fields": False,
+                    "fields": {},
+                },
+            ],
+        )
