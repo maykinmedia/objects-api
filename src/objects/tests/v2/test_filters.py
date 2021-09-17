2275225ba72f33ce0a540105debb6af1f25c677c
Anna Shamray <anna.shamray@maykinmedia.nl>
2021-09-17 11:50:20 +0200

    :white_check_mark: test 'icontains' in 'data_attrs'

--- src/objects/tests/v2/test_filters.py
+++ src/objects/tests/v2/test_filters.py
@@ -312,6 +312,27 @@ class FilterDataAttrsTests(TokenAuthMixin, APITestCase):
             f"http://testserver{reverse('object-detail', args=[record.object.uuid])}",
         )
 
+    def test_filter_icontains_string(self):
+        record = ObjectRecordFactory.create(
+            data={"name": "Something important"}, object__object_type=self.object_type
+        )
+        ObjectRecordFactory.create(
+            data={"name": "Nothing important"}, object__object_type=self.object_type
+        )
+        ObjectRecordFactory.create(data={}, object__object_type=self.object_type)
+
+        response = self.client.get(self.url, {"data_attrs": "name__icontains__some"})
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        data = response.json()["results"]
+
+        self.assertEqual(len(data), 1)
+        self.assertEqual(
+            data[0]["url"],
+            f"http://testserver{reverse('object-detail', args=[record.object.uuid])}",
+        )
+
 
 class FilterDateTests(TokenAuthMixin, APITestCase):
     @classmethod
