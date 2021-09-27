22280e881578365724da4bb8b85e75df4f4a9a8c
Steven Bal <steven@maykinmedia.nl>
2021-09-27 16:23:32 +0200

    :sparkles: Call createinitialsuperuser command post migration

--- src/objects/accounts/apps.py
+++ src/objects/accounts/apps.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 from django.apps import AppConfig, apps
 from django.conf import settings
 from django.contrib.contenttypes.management import create_contenttypes
@@ -19,8 +22,20 @@ def update_admin_index(sender, **kwargs):
     call_command("loaddata", "default_admin_index", verbosity=0)
 
 
+def create_superuser(sender, **kwargs):
+    if "test" in sys.argv:
+        return
+
+    username = os.getenv("SUPERUSER_NAME")
+    password = os.getenv("SUPERUSER_PASSWORD")
+
+    if username and password:
+        call_command("createinitialsuperuser", username, password=password)
+
+
 class AccountsConfig(AppConfig):
     name = "objects.accounts"
 
     def ready(self):
         post_migrate.connect(update_admin_index, sender=self)
+        post_migrate.connect(create_superuser, sender=self)
