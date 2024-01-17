de05ba4ccf2c9a3c929705cee6f517bf74361a2c
Viicos <65306057+Viicos@users.noreply.github.com>
2024-01-17 16:15:24 +0100

    🎨 Format code after `black` update

--- src/objects/core/migrations/0006_auto_20200904_1402.py
+++ src/objects/core/migrations/0006_auto_20200904_1402.py
@@ -5,7 +5,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("core", "0005_remove_object_data"),
     ]
--- src/objects/core/migrations/0015_objectrecord_geometry.py
+++ src/objects/core/migrations/0015_objectrecord_geometry.py
@@ -5,7 +5,6 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("core", "0014_auto_20200915_1740"),
     ]
