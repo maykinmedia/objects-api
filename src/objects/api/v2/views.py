32e4f5b74bfb6f37497c228686bd86236552a046
Sonny Bakker <sonny871@hotmail.com>
2024-07-17 10:31:29 +0200

    Revert "Dummy commit"
    This reverts commit 9fa9c03d18372326f9cf7966aa09198098988813.

--- src/objects/api/v2/views.py
+++ src/objects/api/v2/views.py
@@ -29,7 +29,7 @@ from .filters import ObjectRecordFilterSet
 
 @extend_schema_view(
     list=extend_schema(
-        description=" actual RECORD. "
+        description="Retrieve a list of OBJECTs and their actual RECORD. "
         "The actual record is defined as if the query parameter `date=<today>` was given."
     ),
     retrieve=extend_schema(
