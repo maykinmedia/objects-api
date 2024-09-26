d38668c5f2e970b27ec3e5059eeb8a109a171dad
Anna Shamray <anna.shamray@maykinmedia.nl>
2024-09-26 15:45:48 +0200

    :fire: [#453] remove v1

--- bin/generate_schema.sh
+++ bin/generate_schema.sh
@@ -4,18 +4,9 @@
 #
 # Run this script from the root of the repository
 
-if [ "$1" = "" ]; then
-    echo "You need to pass a version in the first argument"
-    exit 1
-fi
 
-if [ "$1" != "v1" ] && [ "$1" != "v2" ]; then
-    echo "You need to pass a correct version in the first argument. Available values: v1, v2"
-    exit 1
-fi
+export SCHEMA_PATH=src/objects/api/v2/openapi.yaml
 
-export SCHEMA_PATH=src/objects/api/$1/openapi.yaml
+OUTPUT_FILE=$1
 
-OUTPUT_FILE=$2
-
-src/manage.py spectacular --file ${OUTPUT_FILE:-$SCHEMA_PATH} --validate --api-version $1
+src/manage.py spectacular --file ${OUTPUT_FILE:-$SCHEMA_PATH} --validate
--- src/objects/api/urls.py
+++ src/objects/api/urls.py
@@ -1,6 +1,5 @@
 from django.urls import include, path
 
 urlpatterns = [
-    path("v1", include("objects.api.v1.urls", namespace="v1")),
     path("v2", include("objects.api.v2.urls", namespace="v2")),
 ]
