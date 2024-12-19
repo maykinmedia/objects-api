a857827f65bc2a7749438124735a6e837557838f
Daniel Mursa <daniel@maykinmedia.nl>
2024-12-19 11:59:39 +0100

    [#486] Fields type in permissions

--- src/objects/setup_configuration/models/token_auth.py
+++ src/objects/setup_configuration/models/token_auth.py
@@ -1,3 +1,4 @@
+from django_setup_configuration.fields import DjangoModelRef
 from django_setup_configuration.models import ConfigurationModel
 from pydantic import UUID4, Field
 
@@ -6,7 +7,9 @@ from objects.token.models import Permission, TokenAuth
 
 class TokenAuthPermissionConfigurationModel(ConfigurationModel):
     object_type: UUID4
-    fields: dict | None = Field(default=None)
+    fields: dict[str, list[str]] | None = DjangoModelRef(
+        Permission, "fields", default=None
+    )
 
     class Meta:
         django_model_refs = {
