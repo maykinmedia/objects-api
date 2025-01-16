7f27aa3f5e98be1ce228ec263a4202c82287f612
Steven Bal <steven@maykinmedia.nl>
2025-01-16 11:08:27 +0100

    Test

--- bin/setup_configuration.sh
+++ bin/setup_configuration.sh
@@ -4,11 +4,14 @@
 # Run this script from the root of the repository
 
 set -e
-
+echo "ENTRATO"
 if [[ "${RUN_SETUP_CONFIG,,}" =~ ^(true|1|yes)$ ]]; then
+    echo "ENTRATO 1"
     # wait for required services
     /wait_for_db.sh
-
+    
     src/manage.py migrate
     src/manage.py setup_configuration --yaml-file setup_configuration/data.yaml
+    echo "USCITO 1"
 fi
+echo "USCITO "
