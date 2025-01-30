50be3f2a5eec98e474c9d60516c59a315b7749c1
Daniel Mursa <daniel@maykinmedia.nl>
2025-01-30 10:25:40 +0100

    [#464] Create new tests

--- src/objects/tests/admin/test_core_views.py
+++ src/objects/tests/admin/test_core_views.py
@@ -1,21 +1,65 @@
 from django.urls import reverse
 
+import requests_mock
 from django_webtest import WebTest
 from maykin_2fa.test import disable_admin_mfa
 
 from objects.accounts.tests.factories import UserFactory
+from objects.token.tests.factories import ObjectTypeFactory
+
+from ..utils import mock_objecttype, mock_objecttype_version, mock_service_oas_get
 
 
 @disable_admin_mfa()
+@requests_mock.Mocker()
 class ObjectTypeAdminVersionsTests(WebTest):
 
-    def test_authentication_view(self):
+    def test_valid_response_view(self, m):
+        objecttypes_api = "https://example.com/objecttypes/v1/"
+        object_type = ObjectTypeFactory.create(service__api_root=objecttypes_api)
+        mock_service_oas_get(m, objecttypes_api, "objecttypes")
+        m.get(f"{objecttypes_api}objecttypes", json=[])
+        m.get(object_type.url, json=mock_objecttype(object_type.url))
+        version = mock_objecttype_version(object_type.url, attrs={"jsonSchema": {}})
+        m.get(
+            f"{object_type.url}/versions",
+            json={
+                "count": 1,
+                "next": None,
+                "previous": None,
+                "results": [version],
+            },
+        )
+
+        user = UserFactory.create(is_staff=True, is_superuser=True)
+
+        # object_type exist
+        url = reverse("admin:objecttype_versions", args=[object_type.pk])
+        response = self.app.get(url, user=user)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json), 1)
+
+        # object_type does not exist
+        url = reverse("admin:objecttype_versions", args=[100])
+        response = self.app.get(url, user=user)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json), 0)
+
+    def test_endpoint_unreachable(self, m):
+        user = UserFactory.create(is_staff=True, is_superuser=True)
+        object_type = ObjectTypeFactory.create()
+        url = reverse("admin:objecttype_versions", args=[object_type.pk])
+
+        with self.assertRaises(requests_mock.exceptions.NoMockAddress):
+            self.app.get(url, user=user)
+
+    def test_invalid_authentication_view(self, m):
         url = reverse("admin:objecttype_versions", args=[1])
         response = self.client.get(url)
         redirect_url = f"{reverse('admin:login')}?next={url}"
         self.assertRedirects(response, redirect_url, status_code=302)
 
-    def test_permission_view(self):
+    def test_invalid_permission_view(self, m):
         user = UserFactory.create(is_staff=False, is_superuser=False)
         url = reverse("admin:objecttype_versions", args=[1])
         response = self.app.get(url, user=user, auto_follow=True)
@@ -23,10 +67,3 @@ class ObjectTypeAdminVersionsTests(WebTest):
             response,
             f"You are authenticated as {user.username}, but are not authorized to access this page",
         )
-
-        user.is_staff = True
-        user.save()
-
-        url = reverse("admin:objecttype_versions", args=[1])
-        response = self.app.get(url, user=user)
-        self.assertEqual(response.json, [])
