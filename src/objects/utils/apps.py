90eb4b5a29cb94af039ef796da85d996a46625ad
Steven Bal <steven@maykinmedia.nl>
2025-03-06 16:08:25 +0100

    :memo: [maykinmedia/open-api-framework#115] Regenerate schema

--- src/objects/utils/apps.py
+++ src/objects/utils/apps.py
@@ -1,5 +1,19 @@
 from django.apps import AppConfig
 
+from drf_spectacular.extensions import OpenApiFilterExtension
+
+
+def unregister_camelize_filter_extension():
+    """
+    CamelizeFilterExtension from vng_api_common is loaded automatically
+    and cannot be removed using SPECTACULAR_SETTINGS.
+    """
+    OpenApiFilterExtension._registry = [
+        ext
+        for ext in OpenApiFilterExtension._registry
+        if ext.__name__ != "CamelizeFilterExtension"
+    ]
+
 
 class UtilsConfig(AppConfig):
     name = "objects.utils"
@@ -7,3 +21,5 @@ class UtilsConfig(AppConfig):
     def ready(self):
         from . import checks  # noqa
         from . import oas_extensions  # noqa
+
+        unregister_camelize_filter_extension()
