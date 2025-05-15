3e54911b9f03116354f7791c28d6b9d8d20c60cb
Steven Bal <steven@maykinmedia.nl>
2025-05-15 12:20:53 +0200

    :wrench: [#586] Configure required settings for structlog
    the LOGGING settings from OAF have been duplicated to not force all components to use structlog yet, they should be brought back to OAF once Objects API has properly implemented structlog

--- src/objects/conf/docker.py
+++ src/objects/conf/docker.py
@@ -8,5 +8,6 @@ os.environ.setdefault("DB_PASSWORD", config("DB_PASSWORD", "objects"))
 os.environ.setdefault("DB_HOST", config("DB_HOST", "db"))
 os.environ.setdefault("ENVIRONMENT", "docker")
 os.environ.setdefault("LOG_STDOUT", "yes")
+os.environ.setdefault("LOG_FORMAT_CONSOLE", "json")
 
 from .production import *  # noqa isort:skip
