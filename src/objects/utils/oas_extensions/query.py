051d3e2378d73a5c0ed477906c26f96d45b20b4c
Tim de Beer <tim@maykinmedia.nl>
2025-05-15 09:35:25 +0200

    :heavy_plus_sign: add commonground-api-common dependency  and change filters to filters_backend

--- src/objects/utils/oas_extensions/query.py
+++ src/objects/utils/oas_extensions/query.py
@@ -4,13 +4,13 @@ from drf_spectacular.contrib.django_filters import (
 from drf_spectacular.plumbing import build_basic_type, build_parameter_type
 from drf_spectacular.types import OpenApiTypes
 from drf_spectacular.utils import OpenApiParameter
-from vng_api_common import filters
+from vng_api_common import filters_backend
 
 from ..filters import ObjectTypeFilter
 
 
 class DjangoFilterExtension(_DjangoFilterExtension):
-    target_class = filters.Backend
+    target_class = filters_backend.Backend
     priority = 1
 
     def resolve_filter_field(
