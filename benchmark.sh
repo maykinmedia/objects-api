dcd95f50871b2f51df6f23f6d8ca7fa23a4bc39b
Sonny Bakker <sonny871@hotmail.com>
2025-06-06 12:00:18 +0200

    [#335] add sequential requests to benchmark script

--- benchmark.sh
+++ benchmark.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
-PROCESSES=${PROCESSES:-100}
+REQUESTS=${REQUESTS:-10}
+PROCESSES=${PROCESSES:-0}
 
-echo "Using $PROCESSES processes..."
+printf "Performing sequential requests\n";
+
+for (( idRequest=0; idRequest<REQUESTS; idRequest++ ));
+do
+    OUTPUT_FORMAT="Elapsed time: %{time_total}s\nRequest: $idRequest\n";
+
+    curl \
+        --get \
+        --header 'Authorization: Token secret' \
+        --data 'pageSize=1000' \
+        --data 'type=http://localhost:8001/api/v2/objecttypes/f1220670-8ab7-44f1-a318-bd0782e97662' \
+        --data 'data_attrs=kiemjaar__exact__1234' \
+        --data 'ordering=-record__data__contactmoment__datumContact' \
+        --write-out "$OUTPUT_FORMAT" \
+        --output /dev/null \
+        --silent \
+        http://localhost:8000/api/v2/objects;
+done
+
+printf "Using $PROCESSES processes...\n"
 
 for (( idProcess=0; idProcess<PROCESSES; idProcess++ ));
 do
