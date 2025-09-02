f71f8be3ee04ae9558d6b29e899a6733d442794c
OlhaZahoruiko <olha@maykinmedia.nl>
2025-09-02 16:16:27 +0200

    :sparkles: [maykinmedia/open-api-framework#85] Increase uwsgi and celery worker numbers

--- bin/docker_start.sh
+++ bin/docker_start.sh
@@ -10,8 +10,8 @@ export PGPORT=${DB_PORT:-5432}
 fixtures_dir=${FIXTURES_DIR:-/app/fixtures}
 
 uwsgi_port=${UWSGI_PORT:-8000}
-uwsgi_processes=${UWSGI_PROCESSES:-8}
-uwsgi_threads=${UWSGI_THREADS:-4}
+uwsgi_processes=${UWSGI_PROCESSES:-4}
+uwsgi_threads=${UWSGI_THREADS:-1}
 
 mountpoint=${SUBPATH:-/}
 
