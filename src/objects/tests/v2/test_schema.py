836b52062f8c8decbdd2c9d8892d775ff51cf37b
Floris272 <floris@maykinmedia.nl>
2025-09-25 17:03:29 +0200

    :recycle: [open-api-workflows#30] fix redoc url

--- src/objects/tests/v2/test_schema.py
+++ src/objects/tests/v2/test_schema.py
@@ -5,6 +5,6 @@ from .utils import reverse
 
 
 class APISchemaTest(APITestCase):
-    def test_schema_endoint(self):
+    def test_schema_endpoint(self):
         response = self.client.get(reverse("schema-redoc"))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
