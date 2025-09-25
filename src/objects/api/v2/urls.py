d7df9d4e6134d237f0aab44cf9e6356e0215dee2
Floris272 <floris@maykinmedia.nl>
2025-09-25 16:34:51 +0200

    :recycle: [open-api-workflows#30] move yaml api spec url and add json api spec

--- src/objects/api/v2/urls.py
+++ src/objects/api/v2/urls.py
@@ -1,13 +1,16 @@
 from django.urls import include, path
 
 from drf_spectacular.views import (
-    SpectacularJSONAPIView,
     SpectacularRedocView,
-    SpectacularYAMLAPIView,
 )
 from rest_framework import routers
 
 from .views import ObjectViewSet, PermissionViewSet
+
+from objects.utils.oas_extensions.views import (
+    SpectacularYAMLAPIView,
+    SpectacularJSONAPIView,
+)
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r"objects", ObjectViewSet, basename="object")
@@ -16,16 +19,19 @@ router.register(r"permissions", PermissionViewSet)
 app_name = "v2"
 
 urlpatterns = [
-    path("", SpectacularJSONAPIView.as_view(), name="schema-json"),
     path(
         "/",
         include(
             [
-                # schema
                 path(
-                    "schema/openapi.yaml",
+                    "openapi.yaml",
                     SpectacularYAMLAPIView.as_view(),
-                    name="schema",
+                    name="schema-yaml",
+                ),
+                path(
+                    "openapi.json",
+                    SpectacularJSONAPIView.as_view(),
+                    name="schema-json",
                 ),
                 path(
                     "schema/",
