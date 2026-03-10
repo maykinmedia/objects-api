9cc58d73f2ab235d779348016287ad5245fbef52
OlhaZahoruiko <olha@maykinmedia.nl>
2026-03-10 17:07:58 +0100

    :zap: [maykinmedia/open-api-framework#211] Optimize memory usage (max-requests, flower)

--- bin/celery_flower.sh
+++ bin/celery_flower.sh
@@ -5,4 +5,11 @@ set -e
 # Set defaults for OTEL
 export OTEL_SERVICE_NAME="${OTEL_SERVICE_NAME:-objects-flower}"
 
-exec celery --app objects --workdir src flower
+export FLOWER_MAX_TASKS="${FLOWER_MAX_TASKS:-1000}"
+export FLOWER_MAX_WORKERS="${FLOWER_MAX_WORKERS:-50}"
+
+exec celery \
+    --app objects \
+    --workdir src \
+    --broker "${CELERY_BROKER_URL:-redis://localhost:6379/0}" \
+    flower
